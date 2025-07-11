119b00f1d25e1890dcd6a5b5e7b6020aeb97af7c
Callick <callick1997@gmail.com>
2025-07-11 21:19:32 +0100

    added WeatherService to show weather update into view

--- tourismWebsite/app/Http/Controllers/forPlaceInfoController.php
+++ tourismWebsite/app/Http/Controllers/forPlaceInfoController.php
@@ -4,18 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\PlacesInfo;
+use App\Services\WeatherService;
 
 class forPlaceInfoController extends Controller
 {
     //
-    public function show($id)
+    public function show($id, WeatherService $weatherService)
     {
         $placeinfos = PlacesInfo::findOrFail($id);
+        // Fetch weather data for the place
+        $city = $placeinfos->placeLocation;
+        //fetch weather data using the WeatherService
+        $weather = $weatherService->getWeather($city);
         $recommended = PlacesInfo::where('id', '!=', $id)
                     ->latest('id')
                     ->take(3)
                     ->get(['id', 'placeName', 'placeShortdes', 'image', 'placeLocation']);
-        return view('placeInfo', compact('placeinfos', 'recommended'));
+        return view('placeInfo', compact('placeinfos', 'recommended', 'weather'));
     }
     // perform search based on user input
     public function search(Request $request)
