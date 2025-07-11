dc33046f4283140102f9bb48b8e36f2e442f5b9e
Callick <callick1997@gmail.com>
2025-07-11 21:08:34 +0100

    add a folder with a file for showing weather update

---
+++ tourismWebsite/app/Services/WeatherService.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Services;
+
+use Illuminate\Support\Facades\Http;
+
+class WeatherService
+{
+    public function getWeather($city)
+    {
+        $apiKey = config('services.openweather.key');
+
+        // API endpoint
+        $url = "https://api.openweathermap.org/data/2.5/weather";
+
+        // Send GET request to the OpenWeather API
+        $response = Http::get($url, [
+            'q'     => $city,
+            'appid' => $apiKey,
+            'units' => 'metric' // Celsius
+        ]);
+
+        // If request fails, return null
+        if ($response->failed()) {
+            return null;
+        }
+
+        // Return only the needed weather data
+        return [
+            'temperature' => $response['main']['temp'],
+            //default wind speed unit m/s 'wind_speed'  => $response['wind']['speed'],
+            'wind_speed'  => round($response['wind']['speed'] * 3.6, 2), // Convert to km/h take upto 2 decimal places
+            'description' => $response['weather'][0]['description'],
+            'icon'        => $response['weather'][0]['icon'], // For graphic
+            'humidity'    => $response['main']['humidity'],
+        ];
+    }
+}
