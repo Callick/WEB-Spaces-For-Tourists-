e15a0dda15f65b0590bb936a4480b7544c91f967
Callick <callick1997@gmail.com>
2025-08-03 23:51:41 +0100

    slightly changed the way of auth

--- tourismWebsite/app/Http/Middleware/Authenticate.php
+++ tourismWebsite/app/Http/Middleware/Authenticate.php
@@ -12,6 +12,13 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
-        return $request->expectsJson() ? null : route('login');
+        //return $request->expectsJson() ? null : route('login');
+            // Check if the request expects JSON
+        if ($request->expectsJson()) {
+            return null;
+        }
+
+        // Redirect to OpenAdmin login
+        return '/admin/auth/login';
     }
 }
