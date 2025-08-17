8f967f141d251705c20867c5cf811ecd3dd1a2c4
Callick <callick1997@gmail.com>
2025-08-17 01:10:00 +0100

    added link, style etc

--- tourismWebsite/resources/views/layouts/master.blade.php
+++ tourismWebsite/resources/views/layouts/master.blade.php
@@ -4,10 +4,12 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>@yield('title')</title>
+        @yield('links')
         <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
         <link rel="stylesheet" href="{{ asset('fontawesome/css/all.min.css') }}">
         <link rel="stylesheet" href="{{ asset('css/templatemo-style.css') }}">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        @yield('styles')
 
     <!--
         
@@ -25,6 +27,7 @@
         @include('layouts.footer')
         
 
+        @yield('scripts')
         <script src="{{ asset('js/plugins.js') }}"></script>
         <script>
             $(window).on("load", function() {
